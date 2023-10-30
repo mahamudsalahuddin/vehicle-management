18a371e6284ac6eff6bbcd558ee109b68a7e5dd3
mahamudsalahuddin <mahamudmdsalahuddin@gmail.com>
2023-10-30 17:49:38 +0600

    vehicle availability

--- vehicle_management/vehicle_management/doctype/vehicle_availability/vehicle_availability.js
+++ vehicle_management/vehicle_management/doctype/vehicle_availability/vehicle_availability.js
@@ -1,19 +1,31 @@
-// Copyright (c) 2023, salahuddin and contributors
-// For license information, please see license.txt
-
 frappe.ui.form.on('Vehicle Availability', {
-	// refresh: function(frm) {
-	// 	if (frm.doc.status === 'Port') {
-	// 		frm.set_df_property('port', 'hidden', 0);
-	// 		frm.set_df_property('onship', 'hidden', 1);
-	// 	} else if (frm.doc.status === 'Onship') {
-	// 		frm.set_df_property('port', 'hidden', 1);
-	// 		frm.set_df_property('onship', 'hidden', 0);
-	// 	} else {
-	// 		// Handle other cases if necessary
-	// 		frm.set_df_property('port', 'hidden', 1);
-	// 		frm.set_df_property('onship', 'hidden', 1);
-	// 	}
-		
-	// }
+	status: function(frm) {
+		if(frm.doc.status =="Port"){
+			frm.doc.other_details =""
+		}
+		if(frm.doc.status =="Onship"){
+			frm.doc.port_location =""
+		}
+		if(frm.doc.status =="Inhouse"){
+			frm.doc.port_location =""
+		}
+		if(frm.doc.status =="Workshop"){
+			frm.doc.port_location =""
+		}
+		if(frm.doc.status =="Other"){
+			frm.doc.port_location =""
+		}
+		function emptyfunc(){
+
+		}
+
+			// port_location
+			// shed_number
+			// frm.doc.ship_details== ""
+			// frm.doc.house_address== ""
+			// frm.doc.workshop_address== ""
+			// frm.doc.other_details== ""
+			// house_address 
+			// workshop_address other_details
+	}
 });
--- vehicle_management/vehicle_management/doctype/vehicle_details/vehicle_details.js
+++ vehicle_management/vehicle_management/doctype/vehicle_details/vehicle_details.js
@@ -1,8 +1,12 @@
 // Copyright (c) 2023, salahuddin and contributors
-// For license information, please see license.txt
+// For license information, please see license.
 
 frappe.ui.form.on('Vehicle Details', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		// if (frm.doc.car_model && frm.doc.chassis_number) {
+        //     frm.add_custom_button(__(frm.doc.car_model), () => {
+        //         frappe.set_route("Form", frm.doc.chassis_number, frm.doc.car_model);
+        //     });
+        // }
+	}
 });
