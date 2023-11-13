a6696319d5d9fd90fbb178b4a22f1f557a10f7b3
mahamudsalahuddin <mahamudmdsalahuddin@gmail.com>
2023-11-13 15:39:23 +0600

    ...

--- vehicle_management/vehicle_management/doctype/vehicle_price/vehicle_price.js
+++ vehicle_management/vehicle_management/doctype/vehicle_price/vehicle_price.js
@@ -1,53 +1,55 @@
 frappe.ui.form.on('Vehicle Price', {
 	company_price: function (frm) {
-		// frm.set_value('sale_price', (frm.doc.company_price + frm.doc.customer_price))
-		// frm.refresh_field('sale_price');
 		frm.calculate_sale_price(frm)
+		// It call here for grand total
 		frm.calculate_total_qty_and_amount(frm)
 	},
 	customer_price: function (frm) {
-		// frm.set_value('sale_price', (frm.doc.customer_price + frm.doc.company_price))
-		// frm.refresh_field('sale_price');
 		frm.calculate_sale_price(frm)
+		// It call here for grand total
 		frm.calculate_total_qty_and_amount(frm)
 	},
 
 	setup:(frm) => {
+		// To calculate sale price (customer price + company price)
 		frm.calculate_sale_price=(frm)=>{
-			// if(frm.doc.customer_price != "" && frm.doc.company_price !=""){}
 				frm.set_value('sale_price', (frm.doc.customer_price  + frm.doc.company_price))
 				
 		}
-		frm.calculate_grand_total=(frm)=>{
-			// if(frm.doc.customer_price != "" && frm.doc.company_price !=""){}
-				frm.set_value('sale_price', (frm.doc.sell_price  + frm.doc.company_price))
-			
-		}
+		// To calculate item amount =(quality*rate)
 		frm.calculate_amount_in_child_table = function(frm){
 			frm.doc.table.forEach(item => {
 				item.item_amount = item.item_qty * item.item_rate;
 			})
 			frm.refresh_field('table');
 		}
-
+		// To calculate total quantity, total amount and grand total
 		frm.calculate_total_qty_and_amount = (frm) => {
 			let totalQty = 0, totalAmount = 0;
-			frm.doc.table.forEach(item => {
-				totalQty += item.item_qty;
-				totalAmount += item.item_amount;
-			})
-			let grandTotal = frm.doc.sale_price + totalAmount
-			frm.set_value('total_qty', totalQty)
-			frm.set_value('total_amount', totalAmount)
-			frm.set_value('grand_total', grandTotal)
+			if(frm.doc.table!=undefined){
+				frm.doc.table.forEach(item => {
+					totalQty += item.item_qty;
+					totalAmount += item.item_amount;
+					frm.set_value('total_qty', totalQty)
+					frm.set_value('total_amount', totalAmount)
+					let grandTotal = frm.doc.sale_price + totalAmount
+					frm.set_value('grand_total', grandTotal)
+				})
+			}else{
+				let grandTotal = frm.doc.sale_price 
+				// frm.set_value('total_qty', 0)
+				// frm.set_value('total_amount', 0)
+				frm.set_value('grand_total', grandTotal)
+			}
 		}
+		// To remove items from child table
 		frm.table_items_remove = (frm)=>{
 			frm.calculate_amount_in_child_table(frm);
 			frm.calculate_total_qty_and_amount(frm);
 		}
-		frm.add_sale_price_and_total_amount = (frm)=>{
-			frm.sale_price_plus_total_amount(frm);
-		}
+		// frm.add_sale_price_and_total_amount = (frm)=>{
+		// 	frm.sale_price_plus_total_amount(frm);
+		// }
 		frm.check_items_duplicate = function(frm, row){
 			frm.doc.table.forEach(items =>{
 				if(row.item == '' || row.idx == items.idx){
@@ -84,15 +86,12 @@ frappe.ui.form.on("Other Vehicle Items", {
 		var row = locals[cdt][cdn];
 		frm.table_items_remove(frm);
 	},
-	grand_total: (frm, cdt, cdn) => {
-		var row = locals[cdt][cdn];
-		frm.add_sale_price_and_total_amount(frm);
-	},
+	// grand_total: (frm, cdt, cdn) => {
+	// 	var row = locals[cdt][cdn];
+	// 	frm.add_sale_price_and_total_amount(frm);
+	// },
 	item: (frm, cdt, cdn) => {
 		var row = locals[cdt][cdn];
 		frm.check_items_duplicate(frm, row);
 	}
 });
-
-
-
