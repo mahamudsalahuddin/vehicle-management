3dc1f07c8c765ebd11239cd764eb54283235c647
mahamudsalahuddin <mahamudmdsalahuddin@gmail.com>
2023-11-20 11:23:16 +0600

    function overriding of material request doc

--- vehicle_management/public/js/stock/material_request.js
+++ vehicle_management/public/js/stock/material_request.js
@@ -1,32 +1,21 @@
+// Author: Md Salah Uddin Mahamud
+// Date: 20-11-2-23
+// Description: This is the part of Function Overriding. Add two fields Total Quantity (total_qty) and Amount (amount) below of child table of Material Request Doctype. Then add functionality of both field.
 
 frappe.ui.form.on('Material Request', {
 	setup:(frm) => {
+		// To calculate total amount and quantity
 		frm.calculate_qty=(frm)=>{
             let totalQty=0, amount= 0
-			// frm.set_value('sale_price', (frm.doc.customer_price  + frm.doc.company_price))
             frm.doc.items.map((item)=>{
                 totalQty += item.qty
-                // amount += item.amount
+                amount += item.amount
             })
             frm.set_value('custom_total_quantity', totalQty)
-            // console.log("hello")
+			frm.set_value('custom_total', amount)
 		}
-		// frm.calculate_qty(frm)
 	},
-
-
 });
-
-
-// frappe.ui.form.on('Material Request Item', {
-// 	qty:(frm, cdt, cdn) => {
-//         let row = locals[cdt][cdn];
-// 		frm.calculate_qty(frm)
-// 	},
-    
-
-// });
-
 frappe.ui.form.on("Material Request Item", {
 	qty: (frm, cdt, cdn) => {
 		let row = locals[cdt][cdn];
@@ -36,42 +25,13 @@ frappe.ui.form.on("Material Request Item", {
 		let row = locals[cdt][cdn];
 		frm.calculate_qty(frm);
 	},
+	rate:(frm, cdt, cdn)=>{
+		let row= locals[cdt][cdn]
+		frm.calculate_qty(frm)
+	},
+	// If remove any item the function will call automatically and then update item and quantity fields
+	items_remove:(frm, cdt, cdn)=>{
+		let row= locals[cdt][cdn]
+		frm.calculate_qty(frm)
+	}
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// frappe.ui.form.on("Other Vehicle Items", {
-// 	item_qty: (frm, cdt, cdn) => {
-// 		let row = locals[cdt][cdn];
-// 		frm.calculate_amount_in_child_table(frm);
-// 		frm.calculate_total_qty_and_amount(frm);
-// 	},
-// });
