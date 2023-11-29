6833175e8adcd05c3df7ad058880beeb684628f9
mahamudsalahuddin <mahamudmdsalahuddin@gmail.com>
2023-11-29 16:23:24 +0600

    fetch data usinig  query builder

--- vehicle_management/vehicle_management/doctype/test_filter/test_filter.js
+++ vehicle_management/vehicle_management/doctype/test_filter/test_filter.js
@@ -1,4 +1,4 @@
-// ===========fetch data using frappe.call, for this section I wrote query on test_filter.py=========
+// ===========fetch data using Helper Function (frappe.call), for this section I wrote query on test_filter.py=========
 // frappe.ui.form.on('Test Filter', {
 // 	"onload":function(frm) {
 // 		frm.set_query("purchase_order", function() {
@@ -46,7 +46,57 @@
 // });
 
 
-// ===========fetch data using frappe.db.get_all=========
+// ===========fetch data using Helper Function (frappe.db.get_all)=========
+
+// frappe.ui.form.on('Test Filter', {
+// 	"onload":function(frm) {
+// 		frm.set_query("purchase_order", function() {
+// 			if(!frm.doc.supplier){
+// 				frappe.msgprint("Please first select supplier")
+// 			}
+// 			else{
+// 				return {
+// 					"filters": {
+// 						"supplier": frm.doc.supplier,
+// 						//Here the supplier is match from supplier field of purchase order doctype
+// 					}
+// 				};
+// 			}
+			
+// 		});
+// 	},
+// 	purchase_order:function(frm){
+// 		let purchase_order = frm.doc.purchase_order;
+// 		frappe.call({
+// 			method: "vehicle_management.vehicle_management.doctype.test_filter.test_filter.testFunction2",
+// 			args:{
+// 				po: purchase_order,
+// 			}
+// 		})
+// 		.then((r)=>{
+// 					let a = r.message[0][0]
+// 					let b= r.message[1][0]
+// 					const obj = {...a, ...b}
+
+// 					let entry = frm.add_child("purchase_order_item")
+// 					entry.posting_date = obj.transaction_date
+// 					entry.required_date = obj.schedule_date
+// 					entry.item_code = obj.item_code
+// 					entry.item_name = obj.item_name
+// 					entry.uom = obj.uom
+// 					entry.qty = obj.qty
+// 					entry.rate = obj.rate
+// 					entry.amount = obj.amount
+// 					entry.total = obj.total
+// 					entry.grand_total = obj.grand_total
+// 				refresh_field("purchase_order_item")
+// 		})
+// 	}
+// });
+
+
+
+// ===========Fetch data using Query Builder========= 
 
 frappe.ui.form.on('Test Filter', {
 	"onload":function(frm) {
@@ -68,28 +118,27 @@ frappe.ui.form.on('Test Filter', {
 	purchase_order:function(frm){
 		let purchase_order = frm.doc.purchase_order;
 		frappe.call({
-			method: "vehicle_management.vehicle_management.doctype.test_filter.test_filter.testFunction2",
+			method: "vehicle_management.vehicle_management.doctype.test_filter.test_filter.testFunction3",
 			args:{
 				po: purchase_order,
 			}
 		})
 		.then((r)=>{
-					let a = r.message[0][0]
-					let b= r.message[1][0]
-					const obj = {...a, ...b}
-
-					let entry = frm.add_child("purchase_order_item")
-					entry.posting_date = obj.transaction_date
-					entry.required_date = obj.schedule_date
-					entry.item_code = obj.item_code
-					entry.item_name = obj.item_name
-					entry.uom = obj.uom
-					entry.qty = obj.qty
-					entry.rate = obj.rate
-					entry.amount = obj.amount
-					entry.total = obj.total
-					entry.grand_total = obj.grand_total
-				refresh_field("purchase_order_item")
+			console.log(r.message)
+			$.each(r.message, function(_i,e){
+				let entry = frm.add_child("purchase_order_item")
+				entry.posting_date = e.transaction_date
+				entry.required_date = e.schedule_date
+				entry.item_code = e.item_code
+				entry.item_name = e.item_name
+				entry.uom = e.uom
+				entry.qty = e.qty
+				entry.rate = e.rate
+				entry.amount = e.amount
+				entry.total = e.total
+				entry.grand_total = e.grand_total
+			})
+			refresh_field("purchase_order_item")
 		})
 	}
 });
--- vehicle_management/vehicle_management/doctype/test_filter/test_filter.py
+++ vehicle_management/vehicle_management/doctype/test_filter/test_filter.py
@@ -4,15 +4,39 @@ from frappe.model.document import Document
 class TestFilter(Document):
 	pass
 
+# ===========Fetch data using Helper Function (frappe.call)=========
 # @frappe.whitelist()
 # def testFunction(po):
 # 	query = frappe.db.sql(f""" SELECT po.name, po.transaction_date, po.schedule_date, po.total, po.grand_total, poi.item_code, poi.item_name, poi.uom, poi.qty, poi.rate, poi.amount FROM `tabPurchase Order` AS po JOIN `tabPurchase Order Item` AS poi ON poi.parent = po.name WHERE po.name= '{po}' """, as_dict=True)
 # 	return query
 
-@frappe.whitelist()
-def testFunction2(po):
-	query_for_PO = frappe.db.get_all('Purchase Order', {"name": po}, "transaction_date, schedule_date, total, grand_total")
-	query_for_POI = frappe.db.get_all('Purchase Order Item', {"parent": po}, "item_code, item_name, uom, qty, rate, amount")
-	query= query_for_PO + query_for_POI
 
-	return query_for_PO, query_for_POI
+# // ===========Fetch data using Helper Function  (frappe.db.get_all)=========
+# @frappe.whitelist()
+# def testFunction2(po):
+# 	# It for parent doctype (Purchase Order)
+# 	query_for_PO = frappe.db.get_all('Purchase Order', {"name": po}, "transaction_date, schedule_date, total, grand_total")
+
+# 	# It for Child Table (Purchase Order Item) of Purchase Order doctype
+# 	query_for_POI = frappe.db.get_all('Purchase Order Item', {"parent": po}, "item_code, item_name, uom, qty, rate, amount")
+# 	query= query_for_PO + query_for_POI
+
+# 	return query_for_PO, query_for_POI
+
+
+# // ===========Fetch data using Query Builde========= 
+import frappe
+from frappe.query_builder import DocType
+
+@frappe.whitelist()
+def testFunction3(po):
+	PurchaseOrder= DocType("Purchase Order")
+	PurchaseOrderItem= DocType("Purchase Order Item")
+	query=(
+		frappe.qb.from_(PurchaseOrder)
+		.join(PurchaseOrderItem)
+		.on(PurchaseOrder.name == PurchaseOrderItem.parent)
+		.select( PurchaseOrder.transaction_date, PurchaseOrder.schedule_date, PurchaseOrderItem.item_code, PurchaseOrderItem.item_name, PurchaseOrderItem.uom, PurchaseOrderItem.qty, PurchaseOrderItem.rate, PurchaseOrderItem.amount, PurchaseOrder.total, PurchaseOrder.grand_total)
+		.where(PurchaseOrder.name == po)
+	).run(as_dict=True)
+	return query
