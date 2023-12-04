6ee26f1202d489dceec5c849a211405853dc7fc2
mahamudsalahuddin <mahamudmdsalahuddin@gmail.com>
2023-12-04 18:08:50 +0600

    set data other doctypes  depends on checkbox

--- vehicle_management/vehicle_management/doctype/vehicle_price/vehicle_price.py
+++ vehicle_management/vehicle_management/doctype/vehicle_price/vehicle_price.py
@@ -5,63 +5,74 @@
 import frappe
 from frappe.model.document import Document
 
+
 class VehiclePrice(Document):
-	# In validate function I call all the neccessary function created by me
-	def validate(self):
-		self.cal_sale_price()
-		self.cal_amount()
-		self.cal_total_amount_total_qty()
-		self.cal_grand_total()
+    # In validate function I call all the neccessary function created by me
+    def validate(self):
+        self.cal_sale_price()
+        self.cal_amount()
+        self.cal_total_amount_total_qty()
+        self.cal_grand_total()
 
-	# For calculate sale price
-	def cal_sale_price(self):
-		if self.company_price and self.customer_price:
-			self.sale_price = self.company_price + self.customer_price
-	
-	# For calculate amount of every row of child table 
-	def cal_amount(self):
-		for item in self.table:
-			item.item_amount = item.item_qty * item.item_rate
-	
-	# Calculate total amount and total quantity
-	def cal_total_amount_total_qty(self):
-		total_qty = total_amount = 0
-		for item in self.table:
-			total_qty += item.item_qty
-			total_amount += item.item_amount
-		self.total_qty = total_qty
-		self.total_amount = total_amount
+    # For calculate sale price
+    def cal_sale_price(self):
+        if self.company_price and self.customer_price:
+            self.sale_price = self.company_price + self.customer_price
 
-	# For calculate grand total
-	def cal_grand_total(self):
-		if self.sale_price or self.total_amount:
-			self.grand_total= self.sale_price + self.total_amount
+    # For calculate amount of every row of child table
+    def cal_amount(self):
+        for item in self.table:
+            item.item_amount = item.item_qty * item.item_rate
 
-	def on_submit(self):
-		chassis_number = self.vehicle_chassis_no
-		vehicle_details_doc = frappe.get_doc("Vehicle Details", chassis_number)
-		vehicle_details_doc.customer = self.customer
-		vehicle_details_doc.is_sold = self.is_sold 
-		vehicle_details_doc.save()
+    # Calculate total amount and total quantity
+    def cal_total_amount_total_qty(self):
+        total_qty = total_amount = 0
+        for item in self.table:
+            total_qty += item.item_qty
+            total_amount += item.item_amount
+        self.total_qty = total_qty
+        self.total_amount = total_amount
 
-		# for vehicle availability
-		# vehicle_availability_doc = frappe.get_doc({
-		# 	 'doctype': 'Vehicle Availability',
-    	# 	'vehicle_chassis_no': chassis_number
-		# })
-		# vehicle_availability_doc.customer = self.customer
-		# vehicle_availability_doc.is_sold = self.is_sold 
-		# vehicle_availability_doc.save()
-		# print("=====================================")
-		# print(vehicle_availability_doc.customer)
+    # For calculate grand total
+    def cal_grand_total(self):
+        if self.sale_price or self.total_amount:
+            self.grand_total = self.sale_price + self.total_amount
 
+    def on_submit(self):
+        self.VPriceToVDetails()
+        self.VPriceToVAvailability()
 
-		
-	def on_cancel(self):
-		chassis_number = self.vehicle_chassis_no
-		vehicle_details_doc = frappe.get_doc("Vehicle Details", chassis_number)
-		vehicle_details_doc.customer = ""
-		vehicle_details_doc.is_sold = False 
-		vehicle_details_doc.save()
+    def on_cancel(self):
+        self.VPriceToVDetails()
+        self.VPriceToVAvailability()
 
+    def VPriceToVDetails(self):
+        chassis_number = self.vehicle_chassis_no
+        doc = frappe.get_doc("Vehicle Details", chassis_number)
+        if self.docstatus == 1:
+            doc.customer = self.customer
+            doc.is_sold = self.is_sold
+            doc.save()
+        elif self.docstatus == 2:
+            doc.customer = ""
+            doc.is_sold = False
+            doc.save()
 
+    def VPriceToVAvailability(self):
+        chassis_number = self.vehicle_chassis_no
+        # doc_count = frappe.db.count(
+        #     "Vehicle Availability", {"vehicle_chassis_no": chassis_number}
+        # )
+        doc_name = frappe.db.get_value(
+            "Vehicle Availability", {"vehicle_chassis_no": chassis_number}, "name"
+        )
+        doc = frappe.get_doc("Vehicle Availability", doc_name)
+
+        if self.docstatus == 1:
+            doc.is_sold = self.is_sold
+            doc.customer = self.customer
+            doc.save()
+        elif self.docstatus == 2:
+            doc.is_sold = False
+            doc.customer = ""
+            doc.save()
