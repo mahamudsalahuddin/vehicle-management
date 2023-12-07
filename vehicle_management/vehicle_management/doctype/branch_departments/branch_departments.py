3ea4e078a66726e9d4effb9a4887134be10a6558
mahamudsalahuddin <mahamudmdsalahuddin@gmail.com>
2023-12-07 15:08:21 +0600

    create child table

---
+++ vehicle_management/vehicle_management/doctype/branch_departments/branch_departments.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, salahuddin and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class BranchDepartments(Document):
+	pass
